0bbdddc621bdfa3b142504809785cb8495021399
Ivaylo763 <iedzhurelov24a@gmail.com>
2026-03-24 14:11:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 st.title("Галерия от любими животни")
 
+# Инициализация на списъка
 if "animals" not in st.session_state:
-  st.session_state.animals = []
+    st.session_state.animals = []
 
 st.header("Добави ново животно")
 name = st.text_input("Име на животното")
@@ -11,33 +12,39 @@ description = st.text_area("Описание")
 image_url = st.text_input("URL на картинката")
 
 if st.button("Добави"):
-  if name and description and image_url:
-    st.session_state.animals.append({
-      "име" : name,
-      "описание" : description,
-      "картинка": image_url
-    })
-      st.success(f"{name} е добавено!")
-  else:
-      st.warning("Попълнете всички полета!")
+    if name and description and image_url:
+        st.session_state.animals.append({
+            "име": name,
+            "описание": description,
+            "картинка": image_url
+        })
+        st.success(f"{name} е добавено!")
+    else:
+        st.warning("Попълнете всички полета!")
+
+st.header("Премахни животно")
+remove_name = st.text_input("Име на животното за премахване")
 
 if st.button("Премахни"):
-  for a in st.session_state.animals:
-    if a["име"] == remove_name:
-      st.session_state.animals.remove(a)
-      break
+    removed = False
+    for a in st.session_state.animals:
+        if a["име"] == remove_name:
+            st.session_state.animals.remove(a)
+            removed = True
+            break
 
-  st.success(f"{remove_name} е премахнато")
+    if removed:
+        st.success(f"{remove_name} е премахнато")
+    else:
+        st.warning(f"{remove_name} не е намерено")
 
 st.header("Галерия")
 if st.session_state.animals:
-  cols = st.columns(3)
-  for idx, animal in enumerate(st.session_state.animals):
-    with cols [idx % 3]:
-      st.subheader(animal["име"])
-      st.image(animal["картинка"], use_column_width = True)
-      st.write(animal["описание"])
-
+    cols = st.columns(3)
+    for idx, animal in enumerate(st.session_state.animals):
+        with cols[idx % 3]:
+            st.subheader(animal["име"])
+            st.image(animal["картинка"], use_column_width=True)
+            st.write(animal["описание"])
 else:
-  st.info("Галерията е празна. Добавете животни!")
-      
+    st.info("Галерията е празна. Добавете животни!"
